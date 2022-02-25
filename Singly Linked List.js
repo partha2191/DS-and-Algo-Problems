7f96c0ed6fca36c39542a91f7d6260a2b5b2eb37
Partha Ghosh <p.ghosh2191@gmail.com>
2022-02-25 12:11:02 +0000

    added linked list and doubly linked list

--- Singly Linked List.js
+++ Singly Linked List.js
@@ -3,10 +3,9 @@ class Node {
   constructor(value){
     this.value = value;
     this.next = null;
-    this.prev = null;
   }
 }
-class DoublyLinkedList {
+class LinkedList {
   constructor(value){
     this.head = new Node(value)
     this.tail = this.head
@@ -15,7 +14,6 @@ class DoublyLinkedList {
 
   append(value){
     let node = new Node(value);
-    node.prev = this.tail;
     this.tail.next = node;
     this.tail = node;
     this.length++;
@@ -23,7 +21,6 @@ class DoublyLinkedList {
 
   prepend(value){
     let node = new Node(value);
-    this.head.prev = node;
     node.next = this.head;
     this.head = node;
     this.length++;
@@ -37,7 +34,6 @@ class DoublyLinkedList {
       }
       let node = new Node(value);
       node.next = pre.next
-      node.prev = pre
       pre.next = node
       this.length++
     } else {
@@ -45,7 +41,6 @@ class DoublyLinkedList {
     }
   }
 
-  // remaining
   delete(index){
     if(0 < index && index <= this.length){
       let pre = this.head;
@@ -83,17 +78,31 @@ class DoublyLinkedList {
 
 }
 
-const myLinkedList = new DoublyLinkedList(10);
+const myLinkedList = new LinkedList(10);
 
+myLinkedList.prepend(5)
 myLinkedList.append(16)
 myLinkedList.prepend(20)
-myLinkedList.insert(2, 60)
+myLinkedList.prepend(30)
+myLinkedList.insert(4, 60)
+myLinkedList.printLinkedList()
+myLinkedList.delete(4)
 myLinkedList.printLinkedList()
 myLinkedList.delete(2)
 myLinkedList.printLinkedList()
-// myLinkedList.delete(2)
-// myLinkedList.printLinkedList()
 
-console.log(myLinkedList)
+// Recursively adding numbers
+function plus(x){
+  return function (y){
+    if(y){
+      return plus(x+y)
+    } else {
+      console.log(x)
+      return x;
+    }
+  }
+}
+
+// plus(2)(3)(4)(6)();
 
 
